ee815d36325b965d0f0fa95a7241da69818027ac
danforthpeter <danforthpeter66@gmail.com>
2018-07-25 14:35:59 +0800

    first input

---
+++ NeoContractTest1/Contract1.cs
@@ -0,0 +1,22 @@
+﻿using Neo.SmartContract.Framework;
+using Neo.SmartContract.Framework.Services.Neo;
+using System;
+using System.Numerics;
+using System.Text;
+
+namespace NeoContractTest1
+{
+    public class Contract1 : SmartContract
+    {
+        public static byte[] Main()
+        {
+            byte[] trueByte = new byte[] { 1,2,3 };
+
+            return trueByte;
+        }
+
+
+    }
+
+  
+}
