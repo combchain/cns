f9325db7c59176d8c6195745147dff9656524ae1
danforthpeter <danforthpeter66@gmail.com>
2018-08-19 16:45:16 +0800

    update the cns_domaincenter

--- dapp_cns_domaincenter/dapp_cns_domaincenter.cs
+++ dapp_cns_domaincenter/dapp_cns_domaincenter.cs
@@ -7,77 +7,108 @@ namespace DApp
 {
     public class cns_domaincenter : SmartContract
     {
-
+        const int blockday = 4096
         const string rootDomain = "test";
-        static readonly byte[] InitSuperAdmin = Helper.ToScriptHash("");
+        static readonly byte[] InitSuperAdmin = Helper.ToScriptHash("Ajdf2hdinsmndekif993ndke230n3");;
         public static byte[] rootNameHash()
         {
-            return NameHash(rootDomain);
+            return nameHash(rootDomain);
         }
         public static string rootName()
         {
             return rootDomain;
         }
-
-        delegate object deleResolve(string method, object[] arr);
-        
-        static object resolveFull(string protocol, string[] domainarray)
+        public static object[] getInfo(byte[] cnshash)
         {
-            byte[] hash = NameHash(domainarray[0]);
-            byte[] resolver = Storage.Get(Storage.CurrentContext, "mapresolver".AsByteArray().Concat(hash));
-       
-            for (var i = 1; i < domainarray.Length; i)
+            object[] ret = new object[4];
+            ret[0] = Storage.Get(Storage.CurrentContext, cnshash.Concat(new byte[] { 0x00 }));
+            ret[1] = Storage.Get(Storage.CurrentContext, cnshash.Concat(new byte[] { 0x01 }));
+            ret[2] = Storage.Get(Storage.CurrentContext, cnshash.Concat(new byte[] { 0x02 }));
+            ret[3] = Storage.Get(Storage.CurrentContext, cnshash.Concat(new byte[] { 0x03 }));
+            return ret;
+        }
+        delegate object deleDyncall(string method, object[] arr);
+        
+        static byte[] resolveFull(string protocol, string[] domainarray)
+        {
+            byte[] hash = nameHash(domainarray[0]);
+            var height = Blockchain.GetHeight();
+            for (var i = 1; i < domainarray.Length - 1; i++)
             {
-                hash = NameHashSub(hash, domainarray[i]);
-                var ttl = Storage.Get(Storage.CurrentContext, "mapttl".AsByteArray().Concat(hash)).AsBigInteger();
-                if (ttl < Blockchain.GetHeight()) 
+        
+                byte[] data = (byte[])regcall("getSubOwner", new object[] { hash, subhash });
+                if (data.Length == 0)
                 {
-                    return null;
-                }
+                    return new byte[] { 0x00 };
 
-                if (i == domainarray.Length - 1)
-                {
-                    var resolveCall = (deleResolve)resolver.ToDelegate();
-                    return resolveCall("resolve", new object[] {protocol, hash });
                 }
-                else
+                
+                hash = subhash;
+                var ttl = Storage.Get(Storage.CurrentContext, hash.Concat(new byte[] { 0x03 })).AsBigInteger();
+                if (ttl < height)
                 {
-                    var resolveCall = (deleResolve)resolver.ToDelegate();
-                    if ((int)resolveCall("active", new object[] { hash }) == 1)
-                    {
-                        resolver = Storage.Get(Storage.CurrentContext, "mapresolver".AsByteArray().Concat(hash));//得到子解析器
-                    }
-                    else
-                    {
-                        return null;
-                    }
+                    return new byte[] { 0x00 };
                 }
             }
-            return null;
+            string lastname = domainarray[domainarray.Length - 1];
+            return resolve(protocol, hash, lastname);
         }
         
-        static object resolveQuick(string protocol, byte[] cnshash)
+        static byte[] resolve(string protocol, byte[] cnshash, string subdomain)
         {
-            var ttl = Storage.Get(Storage.CurrentContext, "mapttl".AsByteArray().Concat(cnshash)).AsBigInteger();
-            if (ttl < Blockchain.GetHeight()) 
-            {
-                return null;
-            }
+            var fullhash = nameHashSub(cnshash, subdomain);
 
-            var o = Storage.Get(Storage.CurrentContext, "mapresolvedata".AsByteArray().Concat(cnshash).Concat(protocol.AsByteArray()));
-            return o;
+            var resolver = Storage.Get(Storage.CurrentContext, fullhash.Concat(new byte[] { 0x02 }));
+            if (resolver.Length != 0)
+            {
+                var ttl = Storage.Get(Storage.CurrentContext, cnshash.Concat(new byte[] { 0x03 })).AsBigInteger();
+                if (ttl < Blockchain.GetHeight())
+                {
+                    return new byte[] { 0x00 };
+                }
+                
+                byte[] register = Storage.Get(Storage.CurrentContext, cnshash.Concat(new byte[] { 0x01 }));
+                if (register.Length == 0)
+                {
+                    return new byte[] { 0x00 };
+                }
+                var regcall = (deleDyncall)register.ToDelegate();
+
+                var subhash = nameHashSub(cnshash, subdomain);
+                byte[] data = (byte[])regcall("getSubOwner", new object[] { cnshash, subhash });
+                if (data.Length == 0)
+                {
+                    return new byte[] { 0x00 };
+                }
+
+                var resolveCall = (deleDyncall)resolver.ToDelegate();
+                return resolveCall("resolve", new object[] { protocol, fullhash });
+            }
+            resolver = Storage.Get(Storage.CurrentContext, cnshash.Concat(new byte[] { 0x02 }));
+            if (resolver.Length != 0)
+            {
+                var ttl = Storage.Get(Storage.CurrentContext, cnshash.Concat(new byte[] { 0x03 })).AsBigInteger();
+                if (ttl < Blockchain.GetHeight())
+                {
+                    return new byte[] { 0x00 };
+                }
+                var resolveCall = (deleDyncall)resolver.ToDelegate();
+                return resolveCall("resolve", new object[] { protocol, cnshash });
+            }
+            return new byte[] { 0x00 };
         }
 
-        static object owner_SetOwner(byte[] owner, byte[] cnshash, byte[] newowner)
+        static byte[] owner_SetOwner(byte[] owner, byte[] cnshash, byte[] newowner)
         {
-            var callhash = Neo.SmartContract.Framework.Services.System.ExecutionEngine.CallingScriptHash;
-            var o = Storage.Get(Storage.CurrentContext, "mapowner".AsByteArray().Concat(cnshash));
+            var callhash = ExecutionEngine.CallingScriptHash;
+            var o = Storage.Get(Storage.CurrentContext, cnshash.Concat(new byte[] { 0x00 }));
             if (o.Length == 0 && 
                 InitSuperAdmin.AsBigInteger() == owner.AsBigInteger() && 
                 rootNameHash().AsBigInteger() == cnshash.AsBigInteger() 
                 )
             {
-                Storage.Put(Storage.CurrentContext, "mapowner".AsByteArray().Concat(cnshash), newowner);
+                Storage.Put(Storage.CurrentContext, cnshash.Concat(new byte[] { 0x00 }), newowner);
+                return new byte[] { 0x01 };
             }
             if (
                 callhash.AsBigInteger() == o.AsBigInteger()
@@ -85,102 +116,81 @@ namespace DApp
                 (Runtime.CheckWitness(owner) && o.AsBigInteger() == owner.AsBigInteger())
                 )
             {
-                Storage.Put(Storage.CurrentContext, "mapowner".AsByteArray().Concat(cnshash), newowner);
-                return true;
+                Storage.Put(Storage.CurrentContext, cnshash.Concat(new byte[] { 0x00 }), newowner);
+                return new byte[] { 0x01 };
             }
-            return false;
+            return new byte[] { 0x00 };
         }
-        static object owner_SetController(byte[] owner, byte[] cnshash, byte[] controller)
+        static object owner_SetRegister(byte[] owner, byte[] cnshash, byte[] controller)
         {
             var callhash = Neo.SmartContract.Framework.Services.System.ExecutionEngine.CallingScriptHash;
-            var o = Storage.Get(Storage.CurrentContext, "mapowner".AsByteArray().Concat(cnshash));
+            var o = Storage.Get(Storage.CurrentContext, cnshash.Concat(new byte[] { 0x00 }));
+
             if (
                 callhash.AsBigInteger() == o.AsBigInteger()
                 ||
                 (Runtime.CheckWitness(owner) && o.AsBigInteger() == owner.AsBigInteger())
                 )
             {
-                Storage.Put(Storage.CurrentContext, "mapcontroller".AsByteArray().Concat(cnshash), controller);
-                return true;
-            }
-            return false;
-        }
+                Storage.Put(Storage.CurrentContext, cnshash.Concat(new byte[] { 0x01 }), controller);
+                return new byte[] { 0x01 };
 
-        static object controller_SetSubdomainOwner(byte[] cnshash, string subdomain, byte[] owner)
+            }
+            return new byte[] { 0x00 };
+        }
+        static byte[] owner_SetResolver(byte[] owner, byte[] cnshash, byte[] resolver)
         {
-            var c = Storage.Get(Storage.CurrentContext, "mapcontroller".AsByteArray().Concat(cnshash));
-            if (Helper.AsBigInteger(c) == Helper.AsBigInteger(Neo.SmartContract.Framework.Services.System.ExecutionEngine.CallingScriptHash))
+            var callhash = Neo.SmartContract.Framework.Services.System.ExecutionEngine.CallingScriptHash;
+            var o = Storage.Get(Storage.CurrentContext, cnshash.Concat(new byte[] { 0x00 }));
+            if (
+                callhash.AsBigInteger() == o.AsBigInteger()
+                ||
+                (Runtime.CheckWitness(owner) && o.AsBigInteger() == owner.AsBigInteger())
+                )
             {
-
-                byte[] namehashsub = NameHashSub(cnshash, subdomain);
-                Storage.Put(Storage.CurrentContext, "mapowner".AsByteArray().Concat(namehashsub), owner);
-                return true;
+               Storage.Put(Storage.CurrentContext, cnshash.Concat(new byte[] { 0x02 }), resolver);
+                return new byte[] { 0x01 };
             }
-            return false;
+            return new byte[] { 0x00 };
         }
 
-        static object controller_SetResolver(byte[] cnshash, byte[] resolver)
+        static byte[] register_SetSubdomainOwner(byte[] cnshash, string subdomain, byte[] owner, BigInteger ttl)
         {
-            var c = Storage.Get(Storage.CurrentContext, "mapcontroller".AsByteArray().Concat(cnshash));
-            if (Helper.AsBigInteger(c) == Helper.AsBigInteger(Neo.SmartContract.Framework.Services.System.ExecutionEngine.CallingScriptHash))
+            var ttlself = Storage.Get(Storage.CurrentContext, cnshash.Concat(new byte[] { 0x03 })).AsBigInteger();
+            if (
+                (cnshash.AsBigInteger() != rootNameHash().AsBigInteger())
+                &&
+                ttl > ttlself
+                )
             {
-
-                Storage.Put(Storage.CurrentContext, "mapresolver".AsByteArray().Concat(cnshash), resolver);
-                return true;
+                return new byte[] { 0x00 };
             }
-            return false;
-        }
-        static object controller_SetSubdomainResolver(byte[] cnshash, string subdomain, byte[] resolver)
-        {
-            var c = Storage.Get(Storage.CurrentContext, "mapcontroller".AsByteArray().Concat(cnshash));
-            if (Helper.AsBigInteger(c) == Helper.AsBigInteger(Neo.SmartContract.Framework.Services.System.ExecutionEngine.CallingScriptHash))
+            var register = Storage.Get(Storage.CurrentContext, cnshash.Concat(new byte[] { 0x01 }));
+            if (Helper.AsBigInteger(register) == Helper.AsBigInteger(ExecutionEngine.CallingScriptHash))
             {
-                byte[] namehashsub = NameHashSub(cnshash, subdomain);
-                Storage.Put(Storage.CurrentContext, "mapresolver".AsByteArray().Concat(namehashsub), resolver);
-                return true;
+                byte[] namehashsub = nameHashSub(cnshash, subdomain);
+                Storage.Put(Storage.CurrentContext, namehashsub.Concat(new byte[] { 0x00 }), owner);
+                Storage.Put(Storage.CurrentContext, namehashsub.Concat(new byte[] { 0x03 }), ttl);
+                return new byte[] { 0x01 };
             }
-            return false;
-        }
-
-        static object controller_SetResolveData(byte[] cnshash, string protocol, byte[] data)
-        {
-            var c = Storage.Get(Storage.CurrentContext, "mapcontroller".AsByteArray().Concat(cnshash));
-            if (Helper.AsBigInteger(c) == Helper.AsBigInteger(Neo.SmartContract.Framework.Services.System.ExecutionEngine.CallingScriptHash))
-            {
-
-                Storage.Put(Storage.CurrentContext, "mapresolvedata".AsByteArray().Concat(cnshash).Concat(protocol.AsByteArray()), data);
-                return true;
-            }
-            return false;
-        }
-        static object controller_SetSubdomainResolveData(byte[] cnshash, string subdomain, string protocol, byte[] data)
-        {
-            var c = Storage.Get(Storage.CurrentContext, "mapcontroller".AsByteArray().Concat(cnshash));
-            if (Helper.AsBigInteger(c) == Helper.AsBigInteger(Neo.SmartContract.Framework.Services.System.ExecutionEngine.CallingScriptHash))
-            {
-
-                byte[] namehashsub = NameHashSub(cnshash, subdomain);
-                Storage.Put(Storage.CurrentContext, "mapresolvedata".AsByteArray().Concat(namehashsub).Concat(protocol.AsByteArray()), data);
-                return true;
-            }
-            return false;
+            return new byte[] { 0x00 };
         }
 
-        static byte[] NameHash(string domain)
+        static byte[] nameHash(string domain)
         {
             return SmartContract.Sha256(domain.AsByteArray());
         }
-        static byte[] NameHashSub(byte[] roothash, string subdomain)
+        static byte[] nameHashSub(byte[] roothash, string subdomain)
         {
             var domain = SmartContract.Sha256(subdomain.AsByteArray()).Concat(roothash);
             return SmartContract.Sha256(domain);
         }
-        static byte[] NameHashArray(string[] domainarray)
+        static byte[] nameHashArray(string[] domainarray)
         {
-            byte[] hash = NameHash(domainarray[0]);
+            byte[] hash = nameHash(domainarray[0]);
             for (var i = 1; i < domainarray.Length; i)
             {
-                hash = NameHashSub(hash, domainarray[i]);
+                hash = nameHashSub(hash, domainarray[i]);
             }
             return hash;
         }
@@ -194,9 +204,28 @@ namespace DApp
                 return rootName();
             if (method == "rootNameHash")
                 return rootNameHash();
+            if (method == "getInfo")
+                return getInfo(args[0] as byte[]);
+            if (method == "nameHash")
+                return nameHash(args[0] as string);
+            if (method == "nameHashSub")
+                return nameHashSub(args[0] as byte[], args[1] as string);
+            if (method == "nameHashArray")
+                return nameHashArray(args[0] as string[]);
+            if (method == "resolve")
+                return resolve(args[0] as string, args[1] as byte[], args[2] as string);
+            if (method == "resolveFull")
+                return resolveFull(args[0] as string, args[1] as string[]);
+            if (method == "owner_SetOwner")
+                return owner_SetOwner(args[0] as byte[], args[1] as byte[], args[2] as byte[]);
+            if (method == "owner_SetRegister")
+                return owner_SetRegister(args[0] as byte[], args[1] as byte[], args[2] as byte[]);
+            if (method == "owner_SetResolver")
+                return owner_SetResolver(args[0] as byte[], args[1] as byte[], args[2] as byte[]);
+            if (method == "register_SetSubdomainOwner")
+                return register_SetSubdomainOwner(args[0] as byte[], args[1] as string, args[2] as byte[], (args[3] as byte[]).AsBigInteger());
+            return new byte[] { 0 };
 
-
-            return false;
         }
     }
 
